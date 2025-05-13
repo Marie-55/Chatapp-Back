3ea9abf0c6db5f9bebc196126d12c5eca99aadf9
Bencherif-Ines <ines.bencherif@ensia.edu.dz>
2025-05-13 18:42:21 +0100

    fixes made on authentication and join chatroom

--- Backend/src/controllers/chatroom_controller.py
+++ Backend/src/controllers/chatroom_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, g, json, request, jsonify
+from flask import Blueprint, g, json, request, jsonify, session
 from ..services.chatroom_service import ChatroomService
 
 class ChatroomController:
@@ -9,8 +9,9 @@ class ChatroomController:
         if not data or 'name' not in data or 'password' not in data:
             return jsonify({"success": False, "error": "Name and password are required"}), 400
 
-        #user_id = g.current_user.user_id
-        user_id = "15929b81-65e5-4b6d-a1c4-771810a90625"
+        user = session['current_user']
+        user_id = user.user_id
+        #user_id = "15929b81-65e5-4b6d-a1c4-771810a90625"
         if not user_id:
             return jsonify({"success": False, "error": "User ID is required"}), 400
         name = data["name"]
@@ -35,7 +36,10 @@ class ChatroomController:
 
         if not data or 'chatroomName' not in data or 'passcode' not in data:
             return jsonify({"success": False, "error": "Chatroom name and password are required"}), 400
-        user_id= "1b4d57ee-e692-490b-94db-adae44dae4b1"
+        #user_id= "1b4d57ee-e692-490b-94db-adae44dae4b1"
+        user_id = session.get("current_user").user_id
+        if not user_id: 
+            return jsonify({"success": False, "error": "User ID is required"}), 400
         chatroomName = data["chatroomName"]
         passcode = data["passcode"]
 
@@ -51,8 +55,10 @@ class ChatroomController:
     @staticmethod
     def leave_chatroom(chatroom_id):
         
-        #user_id = g.current_user.user_id
-        user_id = "1b4d57ee-e692-490b-94db-adae44dae4b1"
+        #user_id = "1b4d57ee-e692-490b-94db-adae44dae4b1"
+        user_id = session.get("current_user").user_id
+        if not user_id: 
+            return jsonify({"success": False, "error": "User ID is required"}), 400
         result, status = ChatroomService.leave_chatroom(chatroom_id, user_id)
         if status == 200:
             return jsonify({
@@ -69,8 +75,8 @@ class ChatroomController:
             return jsonify({"success": False, "error": "Chatroom ID is required"}), 400
 
         # Placeholder: you should replace this with actual user ID from auth
-        #user_id = g.current_user.user_id
-        user_id = "15929b81-65e5-4b6d-a1c4-771810a90625"
+        user_id = session.get("current_user").user_id
+        #user_id = "15929b81-65e5-4b6d-a1c4-771810a90625"
         if not user_id: 
             return jsonify({"success": False, "error": "User ID is required"}), 400
         result = ChatroomService.get_chatroom_users(chatroom_id, user_id)
@@ -83,8 +89,8 @@ class ChatroomController:
     def kick_user_from_chatroom(chatroom_id):
         data = request.get_json()
         kicked_user_id = data.get('user_id')
-        #current_user_id = g.current_user.user_id
-        current_user_id = "15929b81-65e5-4b6d-a1c4-771810a90625"
+        current_user_id = session.get("current_user").user_id
+        #current_user_id = "15929b81-65e5-4b6d-a1c4-771810a90625"
         if not chatroom_id:
             return jsonify({"success": False, "error": "Chatroom ID is required"}), 400
         if not kicked_user_id:
@@ -107,8 +113,8 @@ class ChatroomController:
             return jsonify({"success": False, "error": "Chatroom ID is required"}), 400
 
         # Placeholder: you should replace this with actual user ID from auth
-        #user_id = g.current_user.user_id
-        user_id = "15929b81-65e5-4b6d-a1c4-771810a90625"
+        user_id = session.get("current_user").user_id
+        #user_id = "15929b81-65e5-4b6d-a1c4-771810a90625"
         if not user_id:
             return jsonify({"success": False, "error": "User ID is required"}), 400
         result,status = ChatroomService.delete_chatroom(chatroom_id, user_id)
@@ -117,6 +123,24 @@ class ChatroomController:
                 return jsonify({
                     "success": True, 
                     "message": "Chatroom deleted successfully"
+                }), status
+        else:
+            return jsonify({"success": False, "error": result}), status
+        
+    @staticmethod
+    def check_chatroom_name():
+        data = request.get_json()
+
+        if not data or 'chatroomName' not in data:
+            return jsonify({"success": False, "error": "Name is required"}), 400
+
+        name = data["chatroomName"]
+        result, status = ChatroomService.check_chatroom_name(name)
+       
+        if status == 200:
+                return jsonify({
+                    "success": True, 
+                    "message": "Chatroom name is available"
                 }), status
         else:
             return jsonify({"success": False, "error": result}), status
