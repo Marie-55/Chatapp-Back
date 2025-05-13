6fea39f32e99a9bf0452d62c76ede1dd3e1d2ec3
nouchka <120617321+Bencherif-Ines@users.noreply.github.com>
2025-05-13 14:52:16 +0100

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -62,6 +62,7 @@ def create_app():
     CORS(app, resources={
         r"/api/*": {
             "origins": "*",
+            "methods" : ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
                 "allow_headers": ["Content-Type", "Authorization"]
         }
     })
