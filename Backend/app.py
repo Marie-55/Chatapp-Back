00bd06d38d1573344e4182172bf343a4e19ff013
nouchka <120617321+Bencherif-Ines@users.noreply.github.com>
2025-05-14 08:50:44 +0100

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -58,15 +58,10 @@ def home():
 def create_app():
     app = Flask(__name__)
     
-    # Enable CORS
-    # CORS(app, resources={
-    #     r"/api/*": {
-    #         "origins": "*",
-    #         "methods" : ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    #             "allowed_headers": ["Content-Type", "Authorization"]
-    #     }
-    # })
-    CORS(app)
+    CORS(app, resources={r"/api/*": {"origins": "*"}},
+     supports_credentials=True,
+     allow_headers=["Content-Type", "Authorization"],
+     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
     
     # Database configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
